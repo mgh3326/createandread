a4388b6c6b4a3ffdf4ecd1c57f3179af60afc2cb
kwanghyunMoon <mgh3326@naver.com>
2019-12-07 20:16:43 +0900

    Rails Create와 Read 실습기

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,18 @@
+class HomeController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def write
+
+  end
+
+  def create
+    post = Post.new
+    post.title = params[:title]
+    post.content = params[:content]
+    post.save
+
+    redirect_to '/index'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get '/index' => 'home#index'
+  get '/write' => 'home#write'
+  post '/create' => 'home#create'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
